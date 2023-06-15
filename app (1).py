31264bd92203b9c5ce1beb20bfc1d43f0e555cb7
bajrang569 <133362669+bajrang569@users.noreply.github.com>
2023-06-15 09:08:42 +0530

    Add files via upload

---
+++ app (1).py
@@ -0,0 +1,146 @@
+# Core Pkgs
+import streamlit as st 
+import altair as alt
+import plotly.express as px 
+import track_utils
+import sys
+sys.path.append('/path/to/track_utils')
+# EDA Pkgs
+import pandas as pd 
+import numpy as np 
+from datetime import datetime
+import sqlite3
+conn = sqlite3.connect('data1.db')
+c = conn.cursor()
+
+# Utils
+import joblib 
+pipe_lr = joblib.load(open("G:/Data Science Project/Sentiment Analysis/finall.pkl","rb"))
+
+
+# Track Utils
+from track_utils import create_page_visited_table,add_page_visited_details,view_all_page_visited_details,add_prediction_details,view_all_prediction_details,create_emotionclf_table
+
+# Fxn
+def predict_emotions(docx):
+	results = pipe_lr.predict([docx])
+	return results[0]
+
+def get_prediction_proba(docx):
+	results = pipe_lr.predict_proba([docx])
+	return results
+
+#emotions_emoji_dict = {"Positive": "😂", "Negative": "😐", "Neutral": "😔"}
+
+
+
+# Main Application
+def main():
+	st.title("Sentiment Classifier App")
+	menu = ["Home","Monitor","About"]
+	choice = st.sidebar.selectbox("Menu",menu)
+	create_page_visited_table()
+	create_emotionclf_table()
+	if choice == "Home":
+		add_page_visited_details("Home",datetime.now())
+		st.subheader("Home-Sentiment In Text")
+
+		with st.form(key='emotion_clf_form'):
+			raw_text = st.text_area("Type Here")
+			submit_text = st.form_submit_button(label='Submit')
+
+		if submit_text:
+			col1,col2  = st.columns(2)
+
+			# Apply Fxn Here
+			#emotions_emoji_dict = {"1": "😂", "2": "😐", "0": "😔"}
+			prediction = predict_emotions(raw_text)
+			probability = get_prediction_proba(raw_text)
+			
+			add_prediction_details(raw_text,prediction,np.max(probability),datetime.now())
+
+			with col1:
+				st.success("Original Text")
+				st.write(raw_text)
+				#emoji_icon = emotions_emoji_dict[prediction]
+                
+				st.success("prediction")
+				#emoji_icon = emotions_emoji_dict[prediction]
+				st.write("{}".format(prediction))
+				st.write("Confidence:{}".format(np.max(probability)))
+
+
+
+			with col2:
+				st.success("Prediction Probability")
+				#st.write(probability)
+				proba_df = pd.DataFrame(probability,columns=pipe_lr.classes_)
+				# st.write(proba_df.T)
+				proba_df_clean = proba_df.T.reset_index()
+				proba_df_clean.columns = ["emotions","probability"]
+
+				fig = alt.Chart(proba_df_clean).mark_bar().encode(x='emotions',y='probability',color='emotions')
+				st.altair_chart(fig,use_container_width=True)
+
+
+
+	elif choice == "Monitor":
+		add_page_visited_details("Monitor",datetime.now())
+		st.subheader("Monitor App")
+
+		with st.expander("Page Metrics"):
+			page_visited_details = pd.DataFrame(view_all_page_visited_details(),columns=['Pagename','Time_of_Visit'])
+			st.dataframe(page_visited_details)	
+
+			pg_count = page_visited_details['Pagename'].value_counts().rename_axis('Pagename').reset_index(name='Counts')
+			c = alt.Chart(pg_count).mark_bar().encode(x='Pagename',y='Counts',color='Pagename')
+			st.altair_chart(c,use_container_width=True)	
+
+			p = px.pie(pg_count,values='Counts',names='Pagename')
+			st.plotly_chart(p,use_container_width=True)
+
+		with st.expander('Emotion Classifier Metrics'):
+			df_emotions = pd.DataFrame(view_all_prediction_details(),columns=['Rawtext','Prediction','Probability','Time_of_Visit'])
+			st.dataframe(df_emotions)
+
+			prediction_count = df_emotions['Prediction'].value_counts().rename_axis('Prediction').reset_index(name='Counts')
+			pc = alt.Chart(prediction_count).mark_bar().encode(x='Prediction',y='Counts',color='Prediction')
+			st.altair_chart(pc,use_container_width=True)	
+
+
+
+	else:
+		st.subheader("About")
+		add_page_visited_details("About",datetime.now())
+# Fxn
+def create_page_visited_table():
+	c.execute('CREATE TABLE IF NOT EXISTS pageTrackTable(pagename TEXT,timeOfvisit TIMESTAMP)')
+
+def add_page_visited_details(pagename,timeOfvisit):
+	c.execute('INSERT INTO pageTrackTable(pagename,timeOfvisit) VALUES(?,?)',(pagename,timeOfvisit))
+	conn.commit()
+
+def view_all_page_visited_details():
+	c.execute('SELECT * FROM pageTrackTable')
+	data = c.fetchall()
+	return data
+
+
+# Fxn To Track Input & Prediction
+def create_emotionclf_table():
+	c.execute('CREATE TABLE IF NOT EXISTS emotionclfTable(rawtext TEXT,prediction TEXT,probability NUMBER,timeOfvisit TIMESTAMP)')
+
+def add_prediction_details(rawtext,prediction,probability,timeOfvisit):
+	c.execute('INSERT INTO emotionclfTable(rawtext,prediction,probability,timeOfvisit) VALUES(?,?,?,?)',(rawtext,prediction,probability,timeOfvisit))
+	conn.commit()
+
+def view_all_prediction_details():
+	c.execute('SELECT * FROM emotionclfTable')
+	data = c.fetchall()
+	return data
+
+
+
+
+if __name__ == '__main__':
+	main()
